f8d59a116e7f6d59a0552d5b71fc54615951aea7
jicking <jicking.d.bebiro@accenture.com>
2018-12-11 11:52:09 +0800

    init setup vue

---
+++ WayKuratWeb.SimpleVueJs/wwwroot/js/VueSubscriberListComponent.js
@@ -0,0 +1,24 @@
+﻿var subscriberListComponent = new Vue({
+    el: '#subscriber-list-component',
+    data: {
+        subscribers: [],
+        isViewReady: false
+    },
+    methods: {
+        refreshData: function () {
+            this.isViewReady = false;
+            
+            //dummy data for now, will update this later
+            var subscribers = [
+                { name: 'jic', email: 'paytercode@waykurat.com' },
+                { name: 'kin', email: 'monsterart@waykurat.com' }
+            ];
+
+            this.subscribers = subscribers;
+            this.isViewReady = true;
+        }
+    },
+    created: function () {
+        this.refreshData();
+    }
+});
